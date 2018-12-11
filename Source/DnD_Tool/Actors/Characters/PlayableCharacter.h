ccd08f627f7fd5e5f736eee2988b9ab462e06da7
Callan Moore <CallanGameDev@gmail.com>
2018-12-12 12:15:53 +1300

    Create test map -> Set as default start up Create base and playable character C++ files

---
+++ Source/DnD_Tool/Actors/Characters/PlayableCharacter.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Actors/Characters/CharacterBase.h"
+#include "PlayableCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DND_TOOL_API APlayableCharacter : public ACharacterBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
