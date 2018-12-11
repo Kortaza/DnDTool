54d841bfd0fb24b452391a273c309293099c73bd
Callan Moore <CallanGameDev@gmail.com>
2018-12-12 09:46:00 +1300

    Empty C++ Project

---
+++ Source/DnD_Tool/DnD_Tool.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "DnD_Tool.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DnD_Tool, "DnD_Tool" );
