417155e597f519f4c53a6f8896b6413c19d5cdf6
Callan Moore <CallanGameDev@gmail.com>
2018-12-12 15:27:22 +1300

    Base character class setup

--- Source/DnD_Tool/Actors/Characters/PlayableCharacter.cpp
+++ Source/DnD_Tool/Actors/Characters/PlayableCharacter.cpp
@@ -1,7 +1,48 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "PlayableCharacter.h"
+#include <ConstructorHelpers.h>
 
+// Static Variables
+int APlayableCharacter::kGridSize = 100;
+float APlayableCharacter::kGridSpeed = 2.0f * APlayableCharacter::kGridSize;
 
+// Sets default values
+APlayableCharacter::APlayableCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
 
+	//
+	Origin = CreateDefaultSubobject<USceneComponent>(TEXT("Origin"));
+	Origin->SetupAttachment(RootComponent);
+	StatueBase = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StatueBase"));
+	StatueBase->SetupAttachment(Origin);
+	StatueCharacter = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StatueCharacter"));
+	StatueCharacter->SetupAttachment(StatueBase);
+
+	//static ConstructorHelpers::FObjectFinder<UStaticMeshComponent>CubeAsset(TEXT("StaticMesh'/Engine/BasicShapes/Cube.Cube'"));
+	//StatueBase->SetStaticMesh(CubeAsset.Object);
+}
+
+// Called when the game starts or when spawned
+void APlayableCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APlayableCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void APlayableCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
 
--- Source/DnD_Tool/Actors/Characters/PlayableCharacter.h
+++ Source/DnD_Tool/Actors/Characters/PlayableCharacter.h
@@ -3,18 +3,44 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "Actors/Characters/CharacterBase.h"
+#include "GameFramework/Character.h"
 #include "PlayableCharacter.generated.h"
 
-/**
- * 
- */
 UCLASS()
-class DND_TOOL_API APlayableCharacter : public ACharacterBase
+class DND_TOOL_API APlayableCharacter : public APawn
 {
 	GENERATED_BODY()
-	
-	
-	
-	
+
+public:
+	// Sets default values for this character's properties
+	APlayableCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	//
+	int GetGridSize() { return kGridSize; }
+
+public:
+	UPROPERTY(VisibleAnywhere, Category = "Viewport")
+		USceneComponent* Origin;
+
+	UPROPERTY(VisibleAnywhere, Category="Viewport")
+		UStaticMeshComponent* StatueCharacter;
+
+	UPROPERTY(VisibleAnywhere, Category = "Viewport")
+		UStaticMeshComponent* StatueBase;
+
+private:
+	static int kGridSize;
+	static float kGridSpeed;
 };
+
